d97cc58317f51ee8c776abfa141015b7879bb554
Jenia <genia90@gmail.com>
2017-04-15 20:52:44 +0300

    We can now send and add dates/times to the db

--- BoilerController/BoilerController/TestServer/TestServer.py
+++ BoilerController/BoilerController/TestServer/TestServer.py
@@ -2,37 +2,54 @@ from flask import Flask, request
 import sqlite3
 
 app = Flask(__name__)
-pins = { 17: {'name': 'LED', 'state': False}}
+pins = {17: {'name': 'LED', 'state': False}}
 
-db = sqlite3.connect('boiler.db')
 
 @app.route('/settime')
 def set_time():
-	on_time = request.form['ontime']
-	on_date = request.form['ondate']
-	off_time = request.form['offtime']
-	off_date = request.form['offdate']
+    db = sqlite3.connect('boiler.db')
+    pin = request.args['dev']
+    start = request.args['ondate'] + " " + request.args['ontime']
+    end = request.args['offdate'] + " " + request.args['offtime']
+    values = (pin, start, end)
+
+    curs = db.cursor()
+    try:
+        curs.execute("INSERT INTO schedule VALUES (?,?,?);", values)
+        db.commit()
+    except:
+        return 'BAD'
+    db.close()
+    return 'OK'
+
+
+@app.route('/gettimes')
+def get_times():
+    pass
+
 
 @app.route('/setled<num>/<state>')
 def set_led(num, state):
-	num = int(num)
-	if state == '1':
-		pins[num]['state'] = True
-	elif state == '0':
-		pins[num]['state'] = False
-	return 'OK'
+    num = int(num)
+    if state == '1':
+        pins[num]['state'] = True
+    elif state == '0':
+        pins[num]['state'] = False
+    return 'OK'
+
 
 @app.route('/getled<num>')
 def get_led(num):
-	num = int(num)
-	if pins[num]['state'] == True:
-		return 'On'
-	return 'Off'
+    num = int(num)
+    if pins[num]['state'] == True:
+        return 'On'
+    return 'Off'
+
 
 @app.route('/')
 def default_route():
-	return 'Please use correct commands.'
+    return 'Please use correct commands.'
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0:5000')
+    app.run(host='0.0.0.0')
--- BoilerController/BoilerController/ViewModels/MainPageViewModel.cs
+++ BoilerController/BoilerController/ViewModels/MainPageViewModel.cs
@@ -6,17 +6,22 @@ using Xamarin.Forms;
 
 namespace BoilerController.ViewModels
 {
-    class MainPageViewModel : ContentPage
+    class MainPageViewModel : INotifyPropertyChanged
     {
         private readonly string _baseurl = "http://localhost:5000/";
         //private readonly string _baseurl = "http://192.168.1.178:5000/"; // uncomment for production
-        private string _status;
+        private string _status = "Status unavailable";
         private Color _statColor;
+        private DateTime _onDate = DateTime.Now, _offDate = DateTime.Now;
+        private TimeSpan _onTime = DateTime.Now.TimeOfDay,
+            _offTime = DateTime.Now.AddMinutes(45).TimeOfDay;
 
         public MainPageViewModel()
         {
             UpdateProps();
         }
+
+        #region Props
 
         public string Status
         {
@@ -38,10 +43,54 @@ namespace BoilerController.ViewModels
             }
         }
 
-        public DateTime DateTime => DateTime.Now;
+        public DateTime OnDate
+        {
+            get => _onDate;
+            set
+            {
+                _onDate = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OnDate"));
+            }
+        }
+
+        public TimeSpan OnTime
+        {
+            get => _onTime;
+            set
+            {
+                _onTime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OnTime"));
+            }
+        }
+
+        public DateTime OffDate
+        {
+            get => _offDate;
+            set
+            {
+                _offDate = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OffDate"));
+            }
+        }
+
+        public TimeSpan OffTime
+        {
+            get => _offTime;
+            set
+            {
+                _offTime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OffTime"));
+            }
+        }
 
         public RelayCommand SwitchCommand => new RelayCommand(SwitchStatus);
+        public RelayCommand SetTimerCommand => new RelayCommand(SetTimer);
 
+        #endregion
+
+        /// <summary>
+        /// Sends a command to the boiler to switch it's status manually.
+        /// </summary>
         private async void SwitchStatus()
         {
             if (Status == "Off")
@@ -62,11 +111,21 @@ namespace BoilerController.ViewModels
             }
         }
 
+        /// <summary>
+        /// Sends a schedule command to the boiler
+        /// </summary>
         private async void SetTimer()
         {
-            
+            var request =
+                $@"settime?dev=17&ontime={OnTime:hh\:mm}&ondate={OnDate:yyyy-MM-dd}&offtime={OffTime:hh\:mm}&offdate={OffDate:yyyy-MM-dd}";
+
+
+            await HttpHandlerTask(request);
         }
 
+        /// <summary>
+        /// Updates properties on startup
+        /// </summary>
         private async void UpdateProps()
         {
             var response = await HttpHandlerTask("getled17");
@@ -87,6 +146,11 @@ namespace BoilerController.ViewModels
             }
         }
 
+        /// <summary>
+        /// Sends formated request to the boiler server
+        /// </summary>
+        /// <param name="request">Request string to send</param>
+        /// <returns>Return status from the server</returns>
         public async Task<string> HttpHandlerTask(string request)
         {
             HttpResponseMessage response;
@@ -99,6 +163,6 @@ namespace BoilerController.ViewModels
             return await response.Content.ReadAsStringAsync();
         }
         
-        public new event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
