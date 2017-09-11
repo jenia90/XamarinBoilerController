70fb55c130a2e75ec86926e34b0a64c56bb4adb2
Jenia Dysin <jenia90@outlook.com>
2017-09-12 00:08:37 +0300

    DaySelection control now works (though it's still ugly af)

--- BoilerController/BoilerController.Common/Controls/DaySelection.xaml.cs
+++ BoilerController/BoilerController.Common/Controls/DaySelection.xaml.cs
@@ -19,21 +19,12 @@ namespace BoilerController.Common.Controls
         }
 
         public static readonly BindableProperty DaysProperty =
-            BindableProperty.Create("Days", typeof(WeekDay), typeof(DaySelection));
+            BindableProperty.Create("Days", typeof(ObservableCollection<WeekDay>), typeof(DaySelection));
 
         public ObservableCollection<WeekDay> Days
         {
             get => (ObservableCollection<WeekDay>)GetValue(DaysProperty);
-            set
-            {
-                DayLayout.Children.Clear();
-                SetValue(DaysProperty, value);
-                foreach (var weekDay in value)
-                {
-                    var label = new SelectableLabel {Text = weekDay.Day, IsSelected = weekDay.IsSelected};
-                    DayLayout.Children.Add(label);
-                }
-            }
+            set => SetValue(DaysProperty, value);
         }
     }
 }
